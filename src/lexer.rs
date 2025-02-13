8985191641e275fc028951affdb9c58de18dd4d8
Sagar Subedi <sagarsubediofficial@gmail.com>
2025-02-12 19:02:25 -0500

    feat: implement next_token, read_char, and new_token for lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use crate::token::Token;
+use crate::token::{Token, TokenKind};
 
 struct Lexer {
     input: Vec<char>,
@@ -9,11 +9,52 @@ struct Lexer {
 
 impl Lexer {
     pub fn new(input: &str) -> Self {
-        todo!();
+        let mut lexer = Lexer {
+            input: input.chars().collect(),
+            position: 0,
+            read_position: 0,
+            ch: Default::default(),
+        };
+        // read first char and increment read_position
+        lexer.read_char();
+        lexer
     }
 
-    fn next_token(&self) -> Token {
-        todo!()
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = '\0';
+        } else {
+            self.ch = self.input[self.read_position]
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn next_token(&mut self) -> Token {
+        let token = match self.ch {
+            '=' => Lexer::new_token(TokenKind::Assign, self.ch),
+            ';' => Lexer::new_token(TokenKind::Semicolon, self.ch),
+            '(' => Lexer::new_token(TokenKind::Lparen, self.ch),
+            ')' => Lexer::new_token(TokenKind::Rparen, self.ch),
+            ',' => Lexer::new_token(TokenKind::Comma, self.ch),
+            '+' => Lexer::new_token(TokenKind::Plus, self.ch),
+            '{' => Lexer::new_token(TokenKind::Lbrace, self.ch),
+            '}' => Lexer::new_token(TokenKind::Rbrace, self.ch),
+            '\0' => Token {
+                kind: TokenKind::Eof,
+                literal: "".to_string(),
+            },
+            _ => Lexer::new_token(TokenKind::Illegal, self.ch),
+        };
+        self.read_char();
+        token
+    }
+
+    fn new_token(kind: TokenKind, ch: char) -> Token {
+        Token {
+            kind,
+            literal: ch.to_string(),
+        }
     }
 }
 
@@ -64,7 +105,7 @@ mod test {
             },
         ];
 
-        let lexer = Lexer::new(input);
+        let mut lexer = Lexer::new(input);
         for (idx, exp_token) in expected.into_iter().enumerate() {
             let recv_token = lexer.next_token();
             assert_eq!(
